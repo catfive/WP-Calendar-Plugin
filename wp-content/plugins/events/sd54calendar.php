21fd8d525f716f7cedfe3dfd3422b80355cab7a5
John Rork <JohnRork@sd54.org>
2011-04-18 13:15:58 -0500

    started on an events widget

--- wp-content/plugins/events/sd54calendar.php
+++ wp-content/plugins/events/sd54calendar.php
@@ -467,4 +467,59 @@ function my_custom_columns($column)
 	elseif ("event_start_time" == $column) echo date('g:i a', strtotime(get_post_meta($post->ID, "event_time_start", true)));
 	elseif ("event_end_time" == $column) echo date('g:i a', strtotime(get_post_meta($post->ID, "event_time_end", true)));
 }
+
+class UpcomingEvents extends WP_Widget {
+/** constructor */
+function UpcomingEvents() {
+	parent::WP_Widget(false, $name = 'Upcoming Events');	
+}
+
+/** @see WP_Widget::widget */
+function widget($args, $instance) {		
+	extract( $args );
+	$title = apply_filters('widget_title', $instance['title']);
+	$widgetqargs = array(
+		'post_type' => 'events', 
+		'meta_key' => 'event_date_start',
+		'meta_value' => strtotime('now'), 
+		'meta_compare' => '>=',
+		'show_posts'=>5,
+	);
+	$widgetq = new WP_query($widgetqargs);
+	global $post;
+
+?>	
+	<?php echo $before_widget; ?>
+	<?php if ( $title ) echo $before_title . $title . $after_title;?>
+	<ul>
+		<?php while ( $widgetq->have_posts() ): $widgetq->the_post();
+			$EventStartTime = get_post_meta($post->ID, 'event_date_start', true);
+			$EventStartDayInt = date('j', $EventStartTime);?>
+			<li><a href="<?php the_permalink()?>"><?php echo date('m/d', $EventStartTime) . ": "; the_title();?></a></li>
+		<?php endwhile;?>
+	</ul>
+	<?php echo $after_widget; ?>
+	<?php
+}
+
+/** @see WP_Widget::update */
+function update($new_instance, $old_instance) {				
+	$instance = $old_instance;
+	$instance['title'] = strip_tags($new_instance['title']);
+	return $instance;
+}
+
+/** @see WP_Widget::form */
+function form($instance) {				
+	$title = esc_attr($instance['title']);
+	?>
+	<p>
+		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+	</p>
+	<?php 
+}
+
+}
+add_action('widgets_init', create_function('', 'return register_widget("UpcomingEvents");'));
 ?>
